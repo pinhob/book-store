6c5274c3fdfbda6ecc30d2c003241e90cc4398b9
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2025-02-12 20:30:35 +0000

    refactor: move controllers to controller  directory

--- bookstore/src/main/java/com/example/bookstore/controller/AuthorController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/AuthorController.java
@@ -1,4 +1,4 @@
-package com.example.bookstore;
+package com.example.bookstore.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
