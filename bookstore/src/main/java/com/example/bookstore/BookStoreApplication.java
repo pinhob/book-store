934ffa482acb882e0d768297145ccd11e89bd214
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2025-02-12 19:30:46 +0000

    refactor: change project name

--- bookstore/src/main/java/com/example/bookstore/BookStoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.example.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class BookStoreApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
 }
