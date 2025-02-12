741d45a0e4d1e76b0d34c3d736d0bc3f380ef299
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2025-02-12 20:06:38 +0000

    feat: create AuthorController

---
+++ bookstore/src/main/java/com/example/bookstore/AuthorController.java
@@ -0,0 +1,23 @@
+package com.example.bookstore;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class AuthorController {
+    @GetMapping("/author")
+    public String getAuthors() {
+        return "Author controller";
+    }
+    
+
+    @PostMapping("/author")
+    public String createAuthor(@RequestBody String author) {
+        //TODO: process POST request
+        
+        return author;
+    }
+        
+}
