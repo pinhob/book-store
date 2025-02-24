e22746c45aad1385a121ff98440d0bbce2f35992
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2025-02-24 17:27:34 +0000

    feat: create AuthorService

--- bookstore/src/main/java/com/example/bookstore/controller/AuthorController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/AuthorController.java
@@ -1,23 +1,29 @@
 package com.example.bookstore.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bookstore.service.AuthorService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class AuthorController {
+    private AuthorService service;
+
+    public AuthorController(AuthorService service) {
+        this.service = service;
+    }
+
     @GetMapping("/author")
     public String getAuthors() {
         return "Author controller";
     }
     
-
     @PostMapping("/author")
     public String createAuthor(@RequestBody String author) {
-        //TODO: process POST request
-        
-        return author;
+        return service.createAuthor(author);
     }
         
 }
---
+++ bookstore/src/main/java/com/example/bookstore/service/AuthorService.java
@@ -0,0 +1,10 @@
+package com.example.bookstore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+    public String createAuthor(String Author) {
+        return Author;
+    }
+}
